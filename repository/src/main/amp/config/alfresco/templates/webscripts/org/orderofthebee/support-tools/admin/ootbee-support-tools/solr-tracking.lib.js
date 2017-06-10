e2cd5d77de44c8b41b11167a0c2433e13e6bac1c
AFaust <axel.faust@acosix.org>
2017-06-10 18:40:09 +0200

    Extract logic to library (convention to allow jslint to run)

---
+++ repository/src/main/amp/config/alfresco/templates/webscripts/org/orderofthebee/support-tools/admin/ootbee-support-tools/solr-tracking.lib.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C) 2017 Cesar Capillas
+ * Copyright (C) 2017 Order of the Bee
+ *
+ * This file is part of Community Support Tools
+ *
+ * Community Support Tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Community Support Tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Community Support Tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Linked to Alfresco
+ * Copyright (C) 2005-2017 Alfresco Software Limited.
+ */
+function buildPropertyGetter(ctxt)
+{
+    var globalProperties, placeholderHelper, propertyGetter;
+
+    globalProperties = ctxt.getBean('global-properties', Packages.java.util.Properties);
+    placeholderHelper = new Packages.org.springframework.util.PropertyPlaceholderHelper('${', '}', ':', true);
+
+    propertyGetter = function(propertyName, defaultValue)
+    {
+        var propertyValue;
+
+        propertyValue = globalProperties[propertyName];
+        if (propertyValue)
+        {
+            propertyValue = placeholderHelper.replacePlaceholders(propertyValue, globalProperties);
+        }
+
+        // native JS strings are always preferrable
+        if (propertyValue !== undefined && propertyValue !== null)
+        {
+            propertyValue = String(propertyValue);
+        }
+        else if (defaultValue !== undefined)
+        {
+            propertyValue = defaultValue;
+        }
+
+        return propertyValue;
+    };
+
+    return propertyGetter;
+}
+
+/* exported loadSolrSummaryAndStatus */
+function loadSolrSummaryAndStatus()
+{
+    var ctxt, propertyGetter, indexSubsystem, solrContextFactory, solrContext, solrAdminClient, args, trackingSummaryResponse, trackingSummary, trackingStatusResponse, trackingStatus;
+
+    ctxt = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
+    propertyGetter = buildPropertyGetter(ctxt);
+    indexSubsystem = propertyGetter('index.subsystem.name');
+
+    if (/^solr([46])?$/.test(indexSubsystem))
+    {
+        model.solrSubsystemEnabled = true;
+
+        solrContextFactory = ctxt.getBean(indexSubsystem, Packages.org.alfresco.repo.search.impl.solr.SolrChildApplicationContextFactory);
+        solrContext = solrContextFactory.getApplicationContext();
+        solrAdminClient = solrContext
+                .getBean('search.solrAdminHTTPCLient', Packages.org.alfresco.repo.search.impl.solr.SolrAdminHTTPClient);
+
+        args = new Packages.java.util.HashMap();
+
+        args.action = 'SUMMARY';
+        args.wt = 'json';
+        trackingSummaryResponse = solrAdminClient.execute(args);
+
+        args.action = 'STATUS';
+        trackingStatusResponse = solrAdminClient.execute(args);
+
+        trackingSummary = JSON.parse(trackingSummaryResponse).Summary;
+        trackingStatus = JSON.parse(trackingStatusResponse).status;
+
+        model.summary1 = trackingSummary.alfresco;
+        model.summary2 = trackingSummary.archive;
+
+        model.status1 = trackingStatus.alfresco.index;
+        model.status2 = trackingStatus.archive.index;
+    }
+    else
+    {
+        model.solrSubsystemEnabled = false;
+    }
+}
