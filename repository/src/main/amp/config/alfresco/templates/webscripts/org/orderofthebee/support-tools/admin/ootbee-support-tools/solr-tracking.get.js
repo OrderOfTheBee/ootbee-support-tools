7b80e8e796e1cbf8620de3e3e184097ee28af795
AFaust <axel.faust@acosix.org>
2017-06-10 18:40:09 +0200

    Extract logic to library (convention to allow jslint to run)

--- repository/src/main/amp/config/alfresco/templates/webscripts/org/orderofthebee/support-tools/admin/ootbee-support-tools/solr-tracking.get.js
+++ repository/src/main/amp/config/alfresco/templates/webscripts/org/orderofthebee/support-tools/admin/ootbee-support-tools/solr-tracking.get.js
@@ -1,8 +1,9 @@
 <import resource="classpath:alfresco/templates/webscripts/org/alfresco/repository/admin/admin-common.lib.js">
+<import resource="classpath:alfresco/templates/webscripts/org/orderofthebee/support-tools/admin/ootbee-support-tools/solr-tracking.lib.js">
 
 /**
- * Copyright (C) 2016 Axel Faust / Markus Joos
- * Copyright (C) 2016 Order of the Bee
+ * Copyright (C) 2017 Cesar Capillas
+ * Copyright (C) 2017 Order of the Bee
  *
  * This file is part of Community Support Tools
  *
@@ -21,35 +22,10 @@
  */
 /*
  * Linked to Alfresco
- * Copyright (C) 2005-2016 Alfresco Software Limited.
+ * Copyright (C) 2005-2017 Alfresco Software Limited.
  */
 
-var ctxt = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
-var solr4 =  ctxt.getBean('solr4',
-Packages.org.alfresco.repo.search.impl.solr.SolrChildApplicationContextFactory);
-var childCtx = solr4.getApplicationContext();
+loadSolrSummaryAndStatus();
 
-// SOLR Admin HTTP Client
-var adminClient = childCtx.getBean('search.solrAdminHTTPCLient', Packages.org.alfresco.repo.search.impl.solr.SolrAdminHTTPClient);
-
-// Args for SUMMARY report 
-var args = new Packages.java.util.HashMap();
-args['action']  = 'SUMMARY';
-args['wt']      = 'json';
-
-// Args for STATUS report 
-// For numDocs, maxDoc, deletedDocs and Heap Memory
-var args2 = new Packages.java.util.HashMap();
-args2['action'] = 'STATUS';
-args2['wt'] = 'json';
-
-var jsonSummary = JSON.parse(adminClient.execute(args));
-var jsonStatus  = JSON.parse(adminClient.execute(args2));
-
-model.summary1 = jsonSummary.Summary.alfresco;
-model.summary2 = jsonSummary.Summary.archive;
-model.status1  = jsonStatus.status.alfresco.index;
-model.status2  = jsonStatus.status.archive.index;
-
-model.tools     = Admin.getConsoleTools("solr-tracking");
-model.metadata  = Admin.getServerMetaData();
+model.tools = Admin.getConsoleTools("solr-tracking");
+model.metadata = Admin.getServerMetaData();
