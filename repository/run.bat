5d5336254b12ef0c751262c13e457ae7de3be226
AFaust <axel.faust@acosix.org>
2019-07-07 19:35:23 +0200

    Adapt to Alfresco SDK 4

---
+++ repository/run.bat
@@ -0,0 +1,99 @@
+@ECHO OFF
+
+SET COMPOSE_FILE_PATH=%CD%\target\docker\docker-compose.yml
+
+IF [%M2_HOME%]==[] (
+    SET MVN_EXEC=mvn
+)
+
+IF NOT [%M2_HOME%]==[] (
+    SET MVN_EXEC=%M2_HOME%\bin\mvn
+)
+
+IF [%1]==[] (
+    echo "Usage: %0 {build_start|build_start_it_supported|start|stop|purge|tail|build_test|test}"
+    GOTO END
+)
+
+IF %1==build_start (
+    CALL :down
+    CALL :build
+    CALL :start
+    CALL :tail
+    GOTO END
+)
+IF %1==build_start_it_supported (
+    CALL :down
+    CALL :build
+    CALL :prepare_test
+    CALL :start
+    CALL :tail
+    GOTO END
+)
+IF %1==start (
+    CALL :start
+    CALL :tail
+    GOTO END
+)
+IF %1==stop (
+    CALL :down
+    GOTO END
+)
+IF %1==purge (
+    CALL:down
+    CALL:purge
+    GOTO END
+)
+IF %1==tail (
+    CALL :tail
+    GOTO END
+)
+IF %1==build_test (
+    CALL :down
+    CALL :build
+    CALL :prepare_test
+    CALL :start
+    CALL :test
+    CALL :tail_all
+    CALL :down
+    GOTO END
+)
+IF %1==test (
+    CALL :test
+    GOTO END
+)
+echo "Usage: %0 {build_start|start|stop|purge|tail|build_test|test}"
+:END
+EXIT /B %ERRORLEVEL%
+
+:start
+    docker volume create support-tools-repo-acs-volume
+    docker volume create support-tools-repo-db-volume
+    docker volume create support-tools-repo-ass-volume
+    docker-compose -f "%COMPOSE_FILE_PATH%" up --build -d
+EXIT /B 0
+:down
+    if exist "%COMPOSE_FILE_PATH%" (
+        docker-compose -f "%COMPOSE_FILE_PATH%" down
+    )
+EXIT /B 0
+:build
+    call %MVN_EXEC% clean package
+EXIT /B 0
+:tail
+    docker-compose -f "%COMPOSE_FILE_PATH%" logs -f
+EXIT /B 0
+:tail_all
+    docker-compose -f "%COMPOSE_FILE_PATH%" logs --tail="all"
+EXIT /B 0
+:prepare_test
+    call %MVN_EXEC% verify -DskipTests=true
+EXIT /B 0
+:test
+    call %MVN_EXEC% verify
+EXIT /B 0
+:purge
+    docker volume rm -f support-tools-repo-acs-volume
+    docker volume rm -f support-tools-repo-db-volume
+    docker volume rm -f support-tools-repo-ass-volume
+EXIT /B 0
---
+++ run.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+export COMPOSE_FILE_PATH=${PWD}/target/docker/docker-compose.yml
+
+if [ -z "${M2_HOME}" ]; then
+  export MVN_EXEC="mvn"
+else
+  export MVN_EXEC="${M2_HOME}/bin/mvn"
+fi
+
+start() {
+    docker volume create support-tools-acs-volume
+    docker volume create support-tools-db-volume
+    docker volume create support-tools-ass-volume
+    docker-compose -f $COMPOSE_FILE_PATH up --build -d
+}
+
+down() {
+    if [ -f $COMPOSE_FILE_PATH ]; then
+        docker-compose -f $COMPOSE_FILE_PATH down
+    fi
+}
+
+purge() {
+    docker volume rm -f support-tools-acs-volume
+    docker volume rm -f support-tools-db-volume
+    docker volume rm -f support-tools-ass-volume
+}
+
+build() {
+    $MVN_EXEC clean package
+}
+
+tail() {
+    docker-compose -f $COMPOSE_FILE_PATH logs -f
+}
+
+tail_all() {
+    docker-compose -f $COMPOSE_FILE_PATH logs --tail="all"
+}
+
+prepare_test() {
+    $MVN_EXEC verify -DskipTests=true
+}
+
+test() {
+    $MVN_EXEC verify
+}
+
+case "$1" in
+  build_start)
+    down
+    build
+    start
+    tail
+    ;;
+  build_start_it_supported)
+    down
+    build
+    prepare_test
+    start
+    tail
+    ;;
+  start)
+    start
+    tail
+    ;;
+  stop)
+    down
+    ;;
+  purge)
+    down
+    purge
+    ;;
+  tail)
+    tail
+    ;;
+  build_test)
+    down
+    build
+    prepare_test
+    start
+    test
+    tail_all
+    down
+    ;;
+  test)
+    test
+    ;;
+  *)
+    echo "Usage: $0 {build_start|build_start_it_supported|start|stop|purge|tail|build_test|test}"
+esac
