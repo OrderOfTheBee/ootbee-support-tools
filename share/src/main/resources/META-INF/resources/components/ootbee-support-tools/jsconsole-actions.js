8740e8eb407c55d554a06967fd1a8a99687035bc
AFaust <axel.faust@acosix.org>
2021-06-16 14:28:55 +0200

    Restructure / rename / merge JS Console codebase
    - Restructure JS Console code into OOTBee Support Tools specific packages
      to avoid any clashes if installed alongside original addon
    - Merge configuration files where relevant (global properties, Spring
      config, i18n)
    - Adapt action / file / URL name fragments to avoid any clashes if
      installed alongside original addon
    - add long-term and temporary exclusions from JSHint checks
    - apply consistent copyright headers in Java / JS / FTL files
    - apply missing XML prolog

---
+++ share/src/main/resources/META-INF/resources/components/ootbee-support-tools/jsconsole-actions.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2016 - 2021 Order of the Bee
+ *
+ * This file is part of OOTBee Support Tools
+ *
+ * OOTBee Support Tools is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OOTBee Support Tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OOTBee Support Tools. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Linked to Alfresco
+ * Copyright (C) 2005 - 2021 Alfresco Software Limited.
+ * 
+ * This file is part of code forked from the JavaScript Console project
+ * which was licensed under the Apache License, Version 2.0 at the time.
+ * In accordance with that license, the modifications / derivative work
+ * is now being licensed under the LGPL as part of the OOTBee Support Tools
+ * addon.
+ */
+
+(function() {
+    YAHOO.Bubbling.fire("registerAction",
+    {
+        actionName: "onActionOpenInOOTBeeJavaScriptConsole",
+        fn: function JSC_onActionOpenInJavascriptConsole(file) {
+        	window.location = Alfresco.constants.URL_PAGECONTEXT +
+        	    "console/admin-console/ootbee-jsconsole?nodeRef=" + file.nodeRef + "&name=" + file.displayName;
+        }
+    });
+    
+    YAHOO.Bubbling.fire("registerAction",
+    {
+        actionName: "onActionDumpInOOTBeeJavaScriptConsole",
+        fn: function JSC_onActionDumpInJavascriptConsole(file) {
+        	window.location = Alfresco.constants.URL_PAGECONTEXT +
+        	    "console/admin-console/ootbee-jsconsole?nodeRef=" + file.nodeRef + "&name=" + file.displayName+"&dump=true";
+        }
+    });
+})();
